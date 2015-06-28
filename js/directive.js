a5f0051404195ae06a85ae19a999717ecb0ecc2e
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-06-28 13:58:33 +0200

    add just show selected

--- js/directive.js
+++ js/directive.js
@@ -4,10 +4,10 @@ angular.module('locator.accommodation-equipment-chooser', []).directive('accEquC
 
     var template = [
         '<div class="wifi equi-ico pointer tt tt-small" aria-label="wlan" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(wifi)" ng-class="{wifiGrey: containsAccommodation(wifi) == false}"></div>',
-        '<div class="tv equi-ico pointer tt tt-small" aria-label="fernsehen" ng-click="addAccommodationEquipment(tv)" ng-class="{tvGrey: containsAccommodation(tv) == false}"></div>',
-        '<div class="shower equi-ico pointer tt tt-small"  aria-label="dusche/wc" ng-click="addAccommodationEquipment(shower)" ng-class="{showerGrey: containsAccommodation(shower) == false}"></div>',
-        '<div class="meal equi-ico pointer tt tt-small" aria-label="kochmöglichkeiten" ng-click="addAccommodationEquipment(meal)" ng-class="{mealGrey: containsAccommodation(meal) == false}"></div>',
-        '<div class="breakfast equi-ico pointer tt tt-small" aria-label="frühstück" ng-click="addAccommodationEquipment(breakfast)" ng-class="{breakfastGrey: containsAccommodation(breakfast) == false}"></div>',
+        '<div class="tv equi-ico pointer tt tt-small" aria-label="fernsehen" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(tv)" ng-class="{tvGrey: containsAccommodation(tv) == false}"></div>',
+        '<div class="shower equi-ico pointer tt tt-small"  aria-label="dusche/wc" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(shower)" ng-class="{showerGrey: containsAccommodation(shower) == false}"></div>',
+        '<div class="meal equi-ico pointer tt tt-small" aria-label="kochmöglichkeiten" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(meal)" ng-class="{mealGrey: containsAccommodation(meal) == false}"></div>',
+        '<div class="breakfast equi-ico pointer tt tt-small" aria-label="frühstück" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(breakfast)" ng-class="{breakfastGrey: containsAccommodation(breakfast) == false}"></div>',
         '<div class="smoker equi-ico pointer tt tt-small" aria-label="rauchermöglichkeiten" ng-click="addAccommodationEquipment(smoker)" ng-class="{smokerGrey: containsAccommodation(smoker) == false}"></div>',
         '<div class="parking equi-ico pointer tt tt-small" aria-label="parkmöglichkeiten" ng-click="addAccommodationEquipment(parking)" ng-class="{parkingGrey: containsAccommodation(parking) == false}"></div>',
         '<div class="handicapped equi-ico pointer tt tt-small" aria-label="behindertengerecht" ng-click="addAccommodationEquipment(handicapped)" ng-class="{handicappedGrey: containsAccommodation(handicapped) == false}"></div>'
