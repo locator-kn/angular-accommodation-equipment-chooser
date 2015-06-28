5849976ae3101c0922f5e8becd5ccf1748319997
Dennis Moosmann <dennis.moosmann@gmx.de>
2015-06-28 13:59:03 +0200

    add just show selected

--- js/directive.js
+++ js/directive.js
@@ -8,9 +8,9 @@ angular.module('locator.accommodation-equipment-chooser', []).directive('accEquC
         '<div class="shower equi-ico pointer tt tt-small"  aria-label="dusche/wc" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(shower)" ng-class="{showerGrey: containsAccommodation(shower) == false}"></div>',
         '<div class="meal equi-ico pointer tt tt-small" aria-label="kochmöglichkeiten" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(meal)" ng-class="{mealGrey: containsAccommodation(meal) == false}"></div>',
         '<div class="breakfast equi-ico pointer tt tt-small" aria-label="frühstück" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(breakfast)" ng-class="{breakfastGrey: containsAccommodation(breakfast) == false}"></div>',
-        '<div class="smoker equi-ico pointer tt tt-small" aria-label="rauchermöglichkeiten" ng-click="addAccommodationEquipment(smoker)" ng-class="{smokerGrey: containsAccommodation(smoker) == false}"></div>',
-        '<div class="parking equi-ico pointer tt tt-small" aria-label="parkmöglichkeiten" ng-click="addAccommodationEquipment(parking)" ng-class="{parkingGrey: containsAccommodation(parking) == false}"></div>',
-        '<div class="handicapped equi-ico pointer tt tt-small" aria-label="behindertengerecht" ng-click="addAccommodationEquipment(handicapped)" ng-class="{handicappedGrey: containsAccommodation(handicapped) == false}"></div>'
+        '<div class="smoker equi-ico pointer tt tt-small" aria-label="rauchermöglichkeiten" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(smoker)" ng-class="{smokerGrey: containsAccommodation(smoker) == false}"></div>',
+        '<div class="parking equi-ico pointer tt tt-small" aria-label="parkmöglichkeiten" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(parking)" ng-class="{parkingGrey: containsAccommodation(parking) == false}"></div>',
+        '<div class="handicapped equi-ico pointer tt tt-small" aria-label="behindertengerecht" ng-hide="containsAccommodation(wifi) == false && getJustShowSelected() == true" ng-click="addAccommodationEquipment(handicapped)" ng-class="{handicappedGrey: containsAccommodation(handicapped) == false}"></div>'
     ];
 
     return {
